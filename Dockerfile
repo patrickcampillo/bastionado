1a1d48fa141d4d7e6039664bdb15ca63e1b0b990
Patrick Campillo <alucam125@ieselcaminas.org>
2021-03-04 22:25:21 +0100

    Dockerfile OK

--- Dockerfile
+++ Dockerfile
@@ -10,6 +10,7 @@ RUN apt-get update -y && apt-get install apt-utils sudo easy-rsa -y
 RUN adduser patrick --disabled-password --gecos ""
 COPY conf.sh /home/patrick/conf.sh
 COPY vars /home/patrick/vars
-RUN chown -R patrick:patrick /home/patrick
-RUN su patrick && cd /home/patrick && ./conf.sh
-#RUN chown -R patrick.patrick /home/patrick
+RUN cd /home/patrick && bash conf.sh
+RUN chown -R patrick.patrick /home/patrick
+USER patrick
+WORKDIR /home/patrick
--- conf.sh
+++ conf.sh
@@ -1,19 +1,17 @@
 #!/bin/bash
 
-#Inicio de sesión el adminitrador de la AC
-su patrick
-cd
-
 #Creación de la infraestructura de easy-rsa
 mkdir /home/patrick/easy-rsa
 ln -s /usr/share/easy-rsa/* /home/patrick/easy-rsa/
-chmod 700 /home/patrick/easy-rsa
 cd /home/patrick/easy-rsa
 bash easyrsa init-pki
 
 #Creación de entidad certificadora
 cp /home/patrick/vars /home/patrick/easy-rsa/vars
-bash easyrsa build-ca nopass
+sed "3d" /usr/share/easy-rsa/openssl-easyrsa.cnf > /home/patrick/easy-rsa/pki/openssl-easyrsa.cnf
+ln -s /usr/share/easy-rsa/safessl-easyrsa.cnf /home/patrick/easy-rsa/pki/safessl-easyrsa.cnf
+bash easyrsa --batch build-ca nopass
+chmod 700 /home/patrick/easy-rsa
 
 #Borrado de archivos de configuración
 rm /home/patrick/conf.sh
